1f3b3b506c1c69b83e811ad0deb423cf4ebe0752
florisporro <floris@florisporro.com>
2018-12-09 18:10:09 +0100

    InputCard integrated with store

--- lsp/src/components/_inputs/_InputCard.vue
+++ lsp/src/components/_inputs/_InputCard.vue
@@ -2,18 +2,21 @@
 .card.bg-light
   .card-header
     | {{ input.name }}
-    span.ml-1.badge.badge-danger Error
-    span.ml-1.badge.badge-info Triggers active
-    button.btn.btn-sm.btn-outline-default
-      i.fas.fa-edit
   .card-body
     p.card-text
       p Connected to source: {{ sources[0].name }}
       p Mode: {{ input.mode }}
         i.fas.ml-1(:class="modeIcon")
       p Transport: {{ input.transport }}
-      button.btn.btn-sm.btn-outline-primary Copy Push URL
-      button.btn.btn-sm.btn-outline-primary Copy stream key
+      .input-group.mb-3
+        input.form-control(readonly, :placeholder="`${settings.server}live/`")
+        .input-group-append
+          button.btn.btn-sm.btn-outline-primary Copy Push URL
+      
+      .input-group.mb-3
+        input.form-control(readonly, :placeholder="input.streamname")
+        .input-group-append
+          button.btn.btn-sm.btn-outline-primary Copy stream key
 </template>
 
 <script>
@@ -22,6 +25,9 @@ export default {
   computed: {
     sources() {
       return this.$store.state.sources
+    },
+    settings() {
+      return this.$store.state.settings
     },
     modeIcon() {
       if (this.input.mode === 'VOD') return 'fa-hdd'
